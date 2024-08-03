f6b3fbc8af5df369418b47c911c196cc7dcb8293
Inam ullah <113088169+inamullah2627@users.noreply.github.com>
2024-08-03 08:21:56 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JScode as newer version
+
+//alesrt(3 + 3) // we are using node js, not browser
+
+console.log(3 + 3)
+
+console.log("inam")
+
+let name = "inam"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
